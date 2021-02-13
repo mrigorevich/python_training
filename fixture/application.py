122c2fefd0019085c46a19ca8120a54b4c400f3e
Denis <specexp@mail.ru>
2021-02-13 13:47:14 +0300

    Выделен класс SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,12 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 
 class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
 
     def return_to_groups_page(self):
         # return to groups page
@@ -39,15 +36,7 @@ class Application:
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+
 
     def open_home_page(self):
         wd = self.wd
