b887215e05941f6e45567d37267ab600f7bfb76e
Denis <specexp@mail.ru>
2021-02-24 12:49:55 +0300

    logout убран из тестов в conftest

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,9 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="Modified group"))
-    app.session.logout()
+
 
 
 def test_edit_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(header="new GROUP header"))
-    app.session.logout()
