c90b1d9f4041d48684fd47b9db41a6d1fc1967f5
Denis <specexp@mail.ru>
2021-02-24 12:33:40 +0300

    Оптимизирован ввод данных в редактирование группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_edit_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit(Group(name="Modified group", header="new GROUP header", footer="not just a group"))
+    app.group.edit_first_group(Group(name="Modified group"))
+    app.session.logout()
+
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="new GROUP header"))
     app.session.logout()
