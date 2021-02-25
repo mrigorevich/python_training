f81a59fc52c81102179dcefab49dbfd7c9ae56aa
Denis <specexp@mail.ru>
2021-02-25 07:53:35 +0300

    добавлено предусловие в edit_group

--- fixture/group.py
+++ fixture/group.py
@@ -13,14 +13,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -67,4 +60,7 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
-
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,10 +1,16 @@
 from model.group import Group
 
 
+def check_group_count(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+
+
 def test_edit_group_name(app):
+    check_group_count(app)
     app.group.edit_first_group(Group(name="Modified group"))
 
 
-
 def test_edit_group_header(app):
+    check_group_count(app)
     app.group.edit_first_group(Group(header="new GROUP header"))
